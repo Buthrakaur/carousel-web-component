9b92bc66759b30a84b762b8618a561c3cc2a5de3
Filip Kinský <filip@filovo.net>
2024-09-13 12:53:19 +0000

    dont require including CSS file - inject CSS from JS

--- carousel.js
+++ carousel.js
@@ -26,7 +26,7 @@ function initializeCarousel(carousel) {
     for (var i = 2, ref = seats.length; i <= ref; i++) {
       new_seat = next(new_seat);
       new_seat.style.order = i;
-      console.log(new_seat);
+      //console.log(new_seat);
     }
 
     carousel.classList.remove('is-set');
@@ -54,6 +54,13 @@ var observer = new MutationObserver(function (mutationsList, observer) {
   });
 });
 
+
+function injectCSS(styles) {
+  const styleElement = document.createElement('style');
+  styleElement.textContent = styles;
+  document.head.appendChild(styleElement);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Start observing the document body for child elements being added
   observer.observe(document.body, { childList: true, subtree: true });
@@ -62,4 +69,85 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll('.carousel').forEach(function (carousel) {
     initializeCarousel(carousel);
   });
+
+  // inject CSS directly to eliminate the need to include CSS file besides JS
+
+  injectCSS(`
+.wrap {
+  overflow: hidden;
+}
+
+.carousel {
+  display: flex;
+  left: -100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  transform: translateX(100%);
+}
+@media (min-width: 30em) {
+  .carousel {
+    left: -33.33333%;
+    transform: translateX(33.33333%);
+  }
+}
+@media (min-width: 40em) {
+  .carousel {
+    left: -20%;
+    transform: translateX(20%);
+  }
+}
+
+.carousel.is-reversing {
+  transform: translateX(-100%);
+}
+@media (min-width: 30em) {
+  .carousel.is-reversing {
+    transform: translateX(-33.33333%);
+  }
+}
+@media (min-width: 40em) {
+  .carousel.is-reversing {
+    transform: translateX(-20%);
+  }
+}
+
+.carousel.is-set {
+  transform: none;
+  transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+.carousel-seat {
+  flex: 1 0 33.33333%;
+  order: 2;
+  line-height: 100px;
+  text-align: center;
+}
+@media (min-width: 30em) {
+  .carousel-seat {
+    flex-basis: 33.33333%;
+  }
+}
+@media (min-width: 40em) {
+  .carousel-seat {
+    flex-basis: 20%;
+  }
+}
+
+.carousel-seat > img {
+  vertical-align: middle;
+}
+
+.carousel-seat.is-ref {
+  order: 1;
+}
+
+.carousel-seat img {
+  max-width: 150px;
+  max-height: 100px;
+  object-fit: cover;
+}
+    `);
 }); 
+
